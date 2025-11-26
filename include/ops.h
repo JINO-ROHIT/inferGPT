1c3100961c6b12e5b8e53737fedecb890beb8cc0
JINO-ROHIT <find.jinorohit@gmail.com>
2025-11-26 21:35:07 +0300

    greedy sampling + temp scaling

--- include/ops.h
+++ include/ops.h
@@ -3,7 +3,8 @@
 #include "tensor.h"
 
 // Function declarations for operations
-int sample_greedy(const Tensor<1>& logits);
+int sample_greedy(const Tensor<1>& logits, float temperature);
+int temperature_sampling(const Tensor<1>& logits, float temperature);
 float sdot(const float* a, const float* b, int n);
 void saxpy(int n, float a, const float* x, float* y);
 void sxpby(int n, const float* x, float b, float* y);
